72f7ab0e31fb2af22327ab8285679bd2532f95c7
Leo Behe <leobehe@gmail.com>
2018-03-01 10:13:55 -0500

    Add new class SynonymDict to interact with dictionary files.

---
+++ src/blowfish/SynonymDict.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blowfish;
+
+/**
+ * Interacts with the synonym files to find appropriate longer synonyms for
+ * a given word or common phrase.
+ * @author Leo
+ */
+public class SynonymDict {
+    protected String currentToken;
+    
+    public SynonymDict(){
+        
+    }
+    
+}
