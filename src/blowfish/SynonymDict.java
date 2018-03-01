7ccf6c1995e532a204ad01abe5e78c4c94fbd6b1
Leo Behe <leobehe@gmail.com>
2018-03-01 10:29:44 -0500

    Add setDict method shells until synonyms.txt is completed.

--- src/blowfish/SynonymDict.java
+++ src/blowfish/SynonymDict.java
@@ -5,16 +5,50 @@
  */
 package blowfish;
 
+import java.util.ArrayList;
+
 /**
  * Interacts with the synonym files to find appropriate longer synonyms for
  * a given word or common phrase.
  * @author Leo
  */
 public class SynonymDict {
-    protected String currentToken;
+    protected String currentToken;    
+    protected ArrayList<String> dict;
     
     public SynonymDict(){
         
     }
+    
+    /**
+     * Reads in synonym values from synonyms.txt and stores them in
+     * the class's dict value.
+     * @return a boolean that is true if the file was ingested successfully.
+     */
+    protected boolean setDict(){
+        
+        return true;
+    }
+    
+    /**
+     * Reads in synonym values from the provided filepath and stores them 
+     * in the class's dict value.
+     * @param filepath the filepath of the synonyms file that should
+     * be ingested.
+     * @return a boolean that is true if the file was ingested successfully.
+     */
+    protected boolean setDict(String filepath){
+        
+        return true;
+    }
+        
+    public String getCurrentToken() {
+        return currentToken;
+    }
+
+    public void setCurrentToken(String currentToken) {
+        this.currentToken = currentToken;
+    }
+    
     
 }
